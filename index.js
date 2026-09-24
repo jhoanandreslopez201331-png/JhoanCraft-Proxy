e7f3be8f292b9e0b2e3d01ebb3a0a377e47472c0
jhoanandreslopez201331-png <jhoanandreslopez201331@gmail.com>
2026-09-24 15:54:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { EaglerServerProxy } = require('eaglercraft-server');
 const server = new EaglerServerProxy({
     bind: '0.0.0.0',
     port: process.env.PORT || 8081,
-    targetAddress: 'TU_IP_DE_ATERNOS.aternos.me', // Aquí irá la dirección de tu servidor de Aternos
-    targetPort: 12345 // Aquí irá el puerto numérico de tu servidor de Aternos
+    targetAddress:jhoan731.aternos.me
+    targetPort:jhoan731.aternos.me:46730
 });
 
 server.listen();
